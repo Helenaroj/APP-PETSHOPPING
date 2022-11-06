bcb37cab201be78444d17d376e397e65be84e5c3
PRUEBA <mhrojast@gmail.com>
2022-11-06 11:14:52 -0500

    new commint

---
+++ app/src/main/java/com/example/myapplication/Envios.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Envios : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_envios)
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Nosostros.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Nosostros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nosostros)
+    }
+}
