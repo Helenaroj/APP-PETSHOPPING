fdaeab00dc3688c634173d3ba3481eb0bf55c64b
PRUEBA <mhrojast@gmail.com>
2022-11-06 18:56:19 -0500

    new commint

--- app/src/main/java/com/example/myapplication/Splash.kt
+++ app/src/main/java/com/example/myapplication/Splash.kt
@@ -3,11 +3,9 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Theme_MyApplication)
-        Thread.sleep(3000)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
