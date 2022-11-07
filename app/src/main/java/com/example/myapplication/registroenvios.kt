8b40728aead13e451578e82270f6b737b3b25545
PRUEBA <mhrojast@gmail.com>
2022-11-06 20:43:00 -0500

    new commint

--- app/src/main/java/com/example/myapplication/registroenvios.kt
+++ app/src/main/java/com/example/myapplication/registroenvios.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class registroenvios : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registroenvios)
+        setContentView(R.layout.activity_usuariohome)
     }
 }
