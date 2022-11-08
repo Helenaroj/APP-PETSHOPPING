c0bededb4355cfa034d156fe7c8b33ee39bb9dc3
PRUEBA <mhrojast@gmail.com>
2022-11-07 21:52:06 -0500

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/myapplication/Ingresousuario.kt
+++ app/src/main/java/com/example/myapplication/Ingresousuario.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityIngresousuarioBinding
 
 class Ingresousuario : AppCompatActivity() {
+    private lateinit var binding: ActivityIngresousuarioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityIngresousuarioBinding.inflate(layoutInflater)
+        val view= binding.root
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingresousuario)
+    binding.Registrese.setOnClickListener{
+        startActivity(Intent(this,registroclientes::class.java))}
+        Toast.makeText(this,"clic en el boton registrese", Toast.LENGTH_LONG).show()
+
+    binding.Return.setOnClickListener{
+        startActivity(Intent(this,MainActivity::class.java))}
+        Toast.makeText(this,"click en el boton Regresar", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/myapplication/registroclientes.kt
+++ app/src/main/java/com/example/myapplication/registroclientes.kt
@@ -1,15 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.databinding.ActivityRegistroclientesBinding
+import com.example.myapplication.databinding.ActivityUsuariohomeBinding
 
 class registroclientes : AppCompatActivity() {
     private lateinit var binding: ActivityRegistroclientesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding=ActivityRegistroclientesBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registroclientes)
-
+    binding.Registrese2.setOnClickListener{ startActivity(Intent(this,ActivityUsuariohomeBinding::class.java))}
+    binding.return2.setOnClickListener{startActivity(Intent(this,ActivityMainBinding::class.java))}
     }
 }
